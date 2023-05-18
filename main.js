efcddbce2c69006b7e592e07235f4cf0e2c03a64
DANIEL <107085390+Daniel-Alves27@users.noreply.github.com>
2023-05-18 20:59:44 -0300

    update do projeto

--- main.js
+++ main.js
@@ -1,37 +1,43 @@
 const lista = document.getElementById("lista");
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
+itens.forEach((elemento) => {
+    criarElemento(elemento)
+}) 
+
 const form = document.getElementById("novoItem").addEventListener("submit", (evento)=>{
     evento.preventDefault()
 
     const nome = evento.target.elements["nome"];
     const quantidade = evento.target.elements["quantidade"];
 
-    criarElemento(nome.value, quantidade.value);
+    const itemAtual ={
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    };
+
+    criarElemento(itemAtual);
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens",JSON.stringify(itens)); 
 
     nome.value = "";
     quantidade.value = "";
 });
 
-function criarElemento(nome, quantidade) {
+function criarElemento(item) {
     const itemLi = document.createElement("li");
     itemLi.classList.add("item");
 
     const itemNumero = document.createElement("strong");
-    itemNumero.innerHTML = quantidade;
+    itemNumero.innerHTML = item.quantidade;
 
     itemLi.appendChild(itemNumero);
-    itemLi.innerHTML += nome;
+    itemLi.innerHTML += item.nome;
 
     //lista = document.getElementById("lista");
     lista.appendChild(itemLi);
 
-    const itemAtual ={
-        "nome": nome,
-        "quantidade": quantidade
-    };
-
-    itens.push(itemAtual)
-
-    localStorage.setItem("itens",JSON.stringify(itens));   
+     
 };
